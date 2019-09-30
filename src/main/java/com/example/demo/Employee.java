ed881ae216d6033e9187ff46cfadf69ddc93b2bc
sbijukchhe <skbijukchhe@gmail.com>
2019-09-30 15:49:19 -0400

    Handling Form Values the Easy Way

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String SSN;
+    private String DOB;
+
+    public Employee() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", SSN='" + SSN + '\'' +
+                ", DOB=" + DOB +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/demo/SpringBoot203EmployeeFormValuesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot203EmployeeFormValuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot203EmployeeFormValuesApplication.class, args);
+    }
+
+}
